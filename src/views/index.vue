c127ac28f5b7665fc5d5facd1cdc14a8c1821c8e
jiang.wenqi <jiang.wenqi@navercorp.com>
2019-10-15 13:27:33 +0800

    #

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div>
-      欢迎您！ 李四 <a href="#">修改密码</a>
+    <div style="margin: 10px;">
+      <a href="#" style="float: right; font-size: 12px;">修改密码</a><span>欢迎您！ 李四</span>
     </div>
-    <div>
-      查勘币： 100
+    <div style="margin: 10px;">
+      查勘币： 100 <img src="../assets/coin.png" style="margin-top: -3px; vertical-align: middle" width="15" height="15"/>
     </div>
     <van-grid clickable :column-num="1">
       <van-grid-item icon="home-o" text="发起互助" to="/create-help" />
